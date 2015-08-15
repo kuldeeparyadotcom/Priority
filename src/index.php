c207fffc958af97e484a8c6e9d0819ca0f4a85d0
kd <kumararyadotcom@gmail.com>
2015-08-14 23:53:00 -0400

    Initial Commit

---
+++ src/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Priority</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width", initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </head>
+
+  <body>
+    <div class="container-fluid">
+      <div class="page-header">  
+        <p><h1>First Things First!</h1></p>
+      </div>
+    </div>
+    
+    <div class="container-fluid text-right">
+      <button type="button" class="btn-link"><label for="manage_survey">Add Tasks!</label></button>
+    </div>
+
+    <div class="container-fluid">
+    </div>
+ 
+    <div class="container-fluid">
+      <blockquote class="blockquote-reverse">
+        <p class="bg-success">
+        "You've got to find what you love."
+        </p>
+        <footer> 
+          - Steve Jobs
+        </footer>
+      </blockquote>
+    </div>
+  </body>
+
+</html>
---
+++ src/testdbconn.php
@@ -0,0 +1,17 @@
+<?php
+
+
+//Just dummy credentials are provided here
+$servername = "mysql";
+$username = "root";
+$password = "password";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
